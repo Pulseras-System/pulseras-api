80f6a35e3e3abde85e0737dca5d94a8f2544cf1c
NhatMinhz <trinhhoangnhatminh123@gmail.com>
2025-05-15 08:34:17 +0700

    Revert "Nminh"

--- src/main/java/com/pulseras/api/dto/CategoryDto.java
+++
@@ -1,9 +0,0 @@
-package com.pulseras.api.dto;
-
-public class CategoryDto {
-    public String id;
-    public int categoryId;
-    public int productId;
-    public String categoryName;
-    public int status;
-}
