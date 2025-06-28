e3d990e0f56f2d846c47044ed5af38064c61bada
VuQuangMinh1212 <vuquangminh12122004@gmail.com>
2025-06-28 22:23:13 +0700

    Update ImageController.java

--- src/main/java/com/pulseras/api/controller/ImageController.java
+++ src/main/java/com/pulseras/api/controller/ImageController.java
@@ -2,6 +2,7 @@ package com.pulseras.api.controller;
 
 import com.pulseras.api.service.S3Service;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,7 +14,7 @@ public class ImageController {
 
     private final S3Service s3Service;
 
-    @PostMapping("/upload")
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file) {
         String url = s3Service.uploadFile(file);
         return ResponseEntity.ok(url);
