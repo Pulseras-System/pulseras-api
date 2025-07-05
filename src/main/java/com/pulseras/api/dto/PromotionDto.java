870fd5d06ead610f0e70fc4a61355439b88af64f
NhatMinhz <trinhhoangnhatminh123@gmail.com>
2025-07-05 13:44:51 +0700

    Update Promotion and Rating

--- src/main/java/com/pulseras/api/dto/PromotionDto.java
+++ src/main/java/com/pulseras/api/dto/PromotionDto.java
@@ -1,11 +1,21 @@
 package com.pulseras.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PromotionDto {
 
     private String promotionId;
     private String productId;
+    private String productName;
     private String promotionName;
     private String promotionDescription;
     private double discountPercentage;
@@ -14,84 +24,4 @@ public class PromotionDto {
     private int status;
     private LocalDateTime createDate;
     private LocalDateTime lastEdited;
-
-    public String getPromotionId() {
-        return promotionId;
-    }
-
-    public void setPromotionId(String promotionId) {
-        this.promotionId = promotionId;
-    }
-
-    public String getProductId() {
-        return productId;
-    }
-
-    public void setProductId(String productId) {
-        this.productId = productId;
-    }
-
-    public String getPromotionName() {
-        return promotionName;
-    }
-
-    public void setPromotionName(String promotionName) {
-        this.promotionName = promotionName;
-    }
-
-    public String getPromotionDescription() {
-        return promotionDescription;
-    }
-
-    public void setPromotionDescription(String promotionDescription) {
-        this.promotionDescription = promotionDescription;
-    }
-
-    public double getDiscountPercentage() {
-        return discountPercentage;
-    }
-
-    public void setDiscountPercentage(double discountPercentage) {
-        this.discountPercentage = discountPercentage;
-    }
-
-    public LocalDateTime getStartDay() {
-        return startDay;
-    }
-
-    public void setStartDay(LocalDateTime startDay) {
-        this.startDay = startDay;
-    }
-
-    public LocalDateTime getExpireDay() {
-        return expireDay;
-    }
-
-    public void setExpireDay(LocalDateTime expireDay) {
-        this.expireDay = expireDay;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public LocalDateTime getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(LocalDateTime createDate) {
-        this.createDate = createDate;
-    }
-
-    public LocalDateTime getLastEdited() {
-        return lastEdited;
-    }
-
-    public void setLastEdited(LocalDateTime lastEdited) {
-        this.lastEdited = lastEdited;
-    }
 }
